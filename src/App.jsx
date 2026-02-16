c592d59fb1df3fa011590764dbab57ca54a5b5eb
Vikash-malakar <vikashmalakar312@gmail.com>
2026-02-16 20:33:22 +0530

    learn with react topic CURD API

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,17 @@
-import axios  from "axios"
+import { useState } from 'react'
 import './App.css'
-import { useEffect, useState } from "react"
-// AXIOS PACKEAGE
-
-
+import Fetch from './Fetch'
+import Insrt from './insert'
+import {Route ,Routes} from "react-router-dom"
 function App() {
-  let [apidata,setapidata]=useState([])
-
- useEffect(()=>{
-  axios.get('https://jsonplaceholder.typicode.com/comments')
-  .then((res)=>setapidata(res.data))
-
- },[])
-
   return (
     <>
-      <table border="" bgcolor="naviblue">
-        
-          <tr>
-            <th>id</th>
-            <th>name</th>
-            <th>email</th>
-            <th>body</th>
-          </tr>
-        
-      
-          {apidata.map((e) => (
-            <tr key={e.id}>
-              <td>{e.id}</td>
-              <td>{e.name}</td>
-              <td>{e.email}</td>
-              <td>{e.body}</td>
-            </tr>
-          ))}
-        
-      </table>
+    <Routes> 
+      <Route index element={ <Insrt />} />
+      <Route path="/Fetch" element={ <Fetch />}/> 
+       </Routes>
+    
     </>
-
   )
 }
 
---
+++ src/Fetch.jsx
@@ -0,0 +1,13 @@
+function Fetch(){
+
+
+    return(
+
+        <>
+        <h1> insert data </h1>
+        
+        
+          </>
+    )
+}
+export default Fetch
